0bb7a71a43b956f842d18e320da0b076dccb1b10
NeutronStar714 <87866218+NeutronStar714@users.noreply.github.com>
2021-08-15 22:56:32 +0800

    Add files via upload

---
+++ module3-solution/script.js
@@ -0,0 +1,10 @@
+function but1(){
+	document.getElementById('msg1').style.display = "block";
+	document.getElementById('msg2').style.display = "block";
+	document.getElementById('msg3').style.display = "block";
+}
+function but1c(){
+	document.getElementById('msg1').style.display = "none";
+	document.getElementById('msg2').style.display = "none";
+	document.getElementById('msg3').style.display = "none";
+}
